3a35fe6ab8399c1887324ff6c4befa9dc531d934
Michel Graciano <michel.graciano@gmail.com>
2013-06-01 17:11:37 -0300

    Alguns ajustes...

--- src/main/java/jdk8/performance/ForEach.java
+++ src/main/java/jdk8/performance/ForEach.java
@@ -5,19 +5,31 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringJoiner;
 
 public class ForEach {
 
+//    private static final Function<Long, List<BigDecimal>> seeds = i -> {
+//        if (i == 0) {
+//            return new ArrayList<>();
+//        }
+//
+//        final List<BigDecimal> s = seeds.apply(i - 1);
+//        s.add(BigDecimal.valueOf(i));
+//        return s;
+//    };
     public static void main(String[] args) {
-        List<BigDecimal> list = getList();//list from 1 to 100
+        List<BigDecimal> list = getList();
 
-        Duration classic = printClassicForEach(list);
-        Duration streamForEach = printStreamForEach(list);
-        Duration iterableForEach = printIterableForEach(list);
+        for (int i = 0; i < 2; i++) {
+            Duration classic = printClassicForEach(list);
+            Duration streamForEach = printStreamForEach(list);
+            Duration iterableForEach = printIterableForEach(list);
 
-        System.out.println("ClassicForEach: " + classic.toMillis());
-        System.out.println("StreamForEach: " + streamForEach.toMillis());
-        System.out.println("IterableForEach: " + iterableForEach.toMillis());
+            System.out.printf("Classic:  %s ms\n", classic.toMillis());
+            System.out.printf("Stream:   %s ms\n", streamForEach.toMillis());
+            System.out.printf("Iterable: %s ms\n", iterableForEach.toMillis());
+        }
         //Example output:
         //ClassicForEach: 3
         //StreamForEach: 58
@@ -33,26 +45,35 @@ public class ForEach {
     }
 
     private static Duration printClassicForEach(List<BigDecimal> list) {
-        Instant inicio = Instant.now();
+        final StringJoiner sj = new StringJoiner(", ", "[", "]");
+        final Instant inicio = Instant.now();
         for (BigDecimal bigDecimal : list) {
-            System.out.println(bigDecimal.add(BigDecimal.ONE));
+            sj.add(bigDecimal.add(BigDecimal.ONE).toString());
         }
-        return Duration.between(inicio, Instant.now());
+        final Duration duration = Duration.between(inicio, Instant.now());
+        System.out.println(sj.toString());
+        return duration;
     }
 
     private static Duration printStreamForEach(List<BigDecimal> list) {
-        Instant inicio = Instant.now();
+        final StringJoiner sj = new StringJoiner(", ", "[", "]");
+        final Instant inicio = Instant.now();
         list.stream().forEach((bigDecimal) -> {
-            System.out.println(bigDecimal.add(BigDecimal.ONE));
+            sj.add(bigDecimal.add(BigDecimal.ONE).toString());
         });
-        return Duration.between(inicio, Instant.now());
+        final Duration duration = Duration.between(inicio, Instant.now());
+        System.out.println(sj.toString());
+        return duration;
     }
 
     private static Duration printIterableForEach(List<BigDecimal> list) {
-        Instant inicio = Instant.now();
+        final StringJoiner sj = new StringJoiner(", ", "[", "]");
+        final Instant inicio = Instant.now();
         list.forEach((bigDecimal) -> {
-            System.out.println(bigDecimal.add(BigDecimal.ONE));
+            sj.add(bigDecimal.add(BigDecimal.ONE).toString());
         });
-        return Duration.between(inicio, Instant.now());
+        final Duration duration = Duration.between(inicio, Instant.now());
+        System.out.println(sj.toString());
+        return duration;
     }
 }
