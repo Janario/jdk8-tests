1aa9406e7cb7ca9d926a9e5ead42e1b2ee5075a1
Janario Oliveira <janario.oliveira@tecsinapse.com.br>
2013-06-01 15:55:43 -0300

    Initial commit

---
+++ src/main/java/jdk8tests/ForEachTest.java
@@ -0,0 +1,58 @@
+package jdk8tests;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForEachTest {
+
+    public static void main(String[] args) {
+        List<BigDecimal> list = getList();//list from 1 to 100
+
+        Duration classic = printClassicForEach(list);
+        Duration streamForEach = printStreamForEach(list);
+        Duration iterableForEach = printIterableForEach(list);
+
+        System.out.println("ClassicForEach: " + classic.toMillis());
+        System.out.println("StreamForEach: " + streamForEach.toMillis());
+        System.out.println("IterableForEach: " + iterableForEach.toMillis());
+        //Example output:
+        //ClassicForEach: 3
+        //StreamForEach: 58
+        //IterableForEach: 6
+    }
+
+    private static List<BigDecimal> getList() {
+        List<BigDecimal> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(BigDecimal.valueOf(i));
+        }
+        return list;
+    }
+
+    private static Duration printClassicForEach(List<BigDecimal> list) {
+        Instant inicio = Instant.now();
+        for (BigDecimal bigDecimal : list) {
+            System.out.println(bigDecimal.add(BigDecimal.ONE));
+        }
+        return Duration.between(inicio, Instant.now());
+    }
+
+    private static Duration printStreamForEach(List<BigDecimal> list) {
+        Instant inicio = Instant.now();
+        list.stream().forEach((bigDecimal) -> {
+            System.out.println(bigDecimal.add(BigDecimal.ONE));
+        });
+        return Duration.between(inicio, Instant.now());
+    }
+
+    private static Duration printIterableForEach(List<BigDecimal> list) {
+        Instant inicio = Instant.now();
+        list.forEach((bigDecimal) -> {
+            System.out.println(bigDecimal.add(BigDecimal.ONE));
+        });
+        return Duration.between(inicio, Instant.now());
+    }
+}
